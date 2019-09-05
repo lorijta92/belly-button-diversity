a39f096c32389babbc97044f2a8618590117a4db
Lori Ta <lorijta@gmail.com>
2019-09-04 21:30:41 -0700

    gauge chart

--- static/app.js
+++ static/app.js
@@ -2,7 +2,10 @@ function buildMetadata(sample) {
   
   var url = "/metadata/" + sample;
 
+  // Fetch metadata for a sample to  build metadata panel
   d3.json(url).then(function(response) {
+    
+    // Store responses as key-value pairs in JavaScript object
     var dict = [];
 
     for (let [key, value] of Object.entries(response)) {
@@ -12,8 +15,7 @@ function buildMetadata(sample) {
       });
     }
 
-    console.log(dict);
-
+    // Clear any existing data before appending new data
     d3.select("#sample-metadata")
     .html("")
     .selectAll("p")
@@ -24,7 +26,42 @@ function buildMetadata(sample) {
       return `${d.key}: ${d.value}`;
     });
   });
+
+  // Gauge Chart for Frequency of Washes
+  d3.json(url).then(function(response) {
+    console.log(response.WFREQ);
+
+    var trace = {
+      title: {text: "Scrubs per Week"},
+      type: "indicator",
+      mode: "gauge",
+      gauge: {
+        bar: {thickness:0},
+        borderwidth: 0,
+        axis: {
+          range: [0,9],
+          tickmode: "linear"
+        },
+        steps: [
+            {range: [0,1], color: "#f7f2eb"},
+            {range: [1,2], color: "#f3f0e4"},
+            {range: [2,3], color: "#e8e6c8"},
+            {range: [3,4], color: "#e4e8ae"},
+            {range: [4,5], color: "#d4e494"},
+            {range: [5,6], color: "#b6cc8a"},
+            {range: [6,7], color: "#86be7f"},
+            {range: [7,8], color: "#84bb8a"},
+            {range: [8,9], color: "#7fb384"}
+          ]
+      }
+    };
+
+    var data = [trace];
+
+    Plotly.newPlot("gauge", data);
+  });
 }
+
 
 function buildCharts(sample) {
   
@@ -33,6 +70,7 @@ function buildCharts(sample) {
   // Pie Chart
   d3.json(url).then(function(response) {
 
+    // Grab First 10 Samples of Data to Plot
     var trace1 = {
       values:response.sample_values.slice(0,10),
       labels:response.otu_ids.slice(0,10),
@@ -43,12 +81,10 @@ function buildCharts(sample) {
     var data1 = [trace1];
 
     var layout1 = {
-      title: "Top 10 Samples",
-      height: 500,
-      width: 500
+      title: "Top 10 Samples"
     };
 
-    Plotly.plot("pie", data1, layout1);
+    Plotly.newPlot("pie", data1, layout1);
   });
 
   // Bubble Chart
@@ -71,16 +107,13 @@ function buildCharts(sample) {
     var layout2 = {
       title: "Belly Button Diversity",
       xaxis: {title: "OTU ID"},
-      showlegend: false,
-      width: 1200
+      showlegend: false
     };
 
-    Plotly.plot("bubble", data2, layout2);
+    Plotly.newPlot("bubble", data2, layout2);
   });
-
-  // BONUS: Build the Gauge Chart
-  // buildGauge(data.WFREQ);
 }
+
 
 function init() {
   // Grab a reference to the dropdown select element
